6a03c270a04cc35f333798efb2f202d2b804b475
LeeHaii <thangthcskt@gmail.com>
2026-07-03 16:44:47 +0700

    update build and cors
    bild and cors

--- BE/main.py
+++ BE/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes import houses
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,3 +9,11 @@ app.include_router(houses.router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Houses Database API!"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # This allows requests from any website/localhost
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Allows all headers
+)
